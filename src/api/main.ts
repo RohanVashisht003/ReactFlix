e3b2e10fd171fe214eddaf8e155b42465e752656
RohanVashisht003 <sharmarohan544@gmail.com>
2023-08-26 15:07:42 +0530

    Feat: Apis setup

---
+++ src/api/main.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { ROUTES } from "../constants/routes";
+
+const instance = axios.create({
+  baseURL: `${process.env.REACT_APP_SERVER_URL}/api/v1`,
+});
+
+instance.interceptors.request.use((req) => {
+  if (
+    req.url !== ROUTES.HOME.path &&
+    req.url !== ROUTES.SIGNIN.path &&
+    req.url !== ROUTES.SIGNUP.path
+  ) {
+    req.headers.Authorization = `Basic ${localStorage.getItem("netflix-auth")}`;
+  }
+  return req;
+});
+
+export default instance;
