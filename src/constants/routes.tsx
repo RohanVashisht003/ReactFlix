6658b7dc47f88cfa3eff909b896f4ee883fabe19
RohanVashisht003 <sharmarohan544@gmail.com>
2023-08-26 15:13:51 +0530

    Feat: signin page

--- src/constants/routes.tsx
+++ src/constants/routes.tsx
@@ -1,4 +1,5 @@
 import Home from "../pages/Home";
+import SignIn from "../pages/SignIn";
 
 type Routes = {
   [key: string]: {
@@ -12,5 +13,5 @@ export const ROUTES: Routes = {
   HOME: { path: "/", page: <Home />, isProtected: false },
   BROWSE: { path: "/browse", page: <></>, isProtected: true },
   SIGNUP: { path: "/sign-up", page: <></>, isProtected: false },
-  SIGNIN: { path: "/sign-in", page: <></>, isProtected: false },
+  SIGNIN: { path: "/sign-in", page: <SignIn />, isProtected: false },
 };
