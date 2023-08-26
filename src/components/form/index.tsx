8c932d0c177cc23a51318cc5aa05a28dade40fb8
RohanVashisht003 <sharmarohan544@gmail.com>
2023-08-26 15:11:47 +0530

    Feat: form component.

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,5 +1,17 @@
 import { ComponentProp } from "../../types/AllTypes";
-import { Container, Inner, Title } from "./styles/form";
+import {
+  Button,
+  Container,
+  Error,
+  FormGroup,
+  Inner,
+  Input,
+  Link,
+  Spinner,
+  Text,
+  TextSmall,
+  Title,
+} from "./styles/form";
 
 function Form({ children, ...restProps }: ComponentProp) {
   return (
@@ -13,5 +25,44 @@ Form.Title = function FormTitle({ children, ...restProps }: ComponentProp) {
   return <Title {...restProps}>{children}</Title>;
 };
 
+Form.FormGroup = function FormFormGroup({
+  children,
+  ...restProps
+}: ComponentProp) {
+  return <FormGroup {...restProps}>{children}</FormGroup>;
+};
 
+Form.Input = function FormInput({ ...restProps }) {
+  return <Input {...restProps} />;
+};
+Form.Button = function FormButton({ children, ...restProps }: ComponentProp) {
+  return <Button {...restProps}>{children}</Button>;
+};
+
+Form.Text = function FormText({ children, ...restProps }: ComponentProp) {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+Form.TextSmall = function FormTextSmall({
+  children,
+  ...restProps
+}: ComponentProp) {
+  return <TextSmall {...restProps}>{children}</TextSmall>;
+};
+
+Form.Link = function FormLink({ children, to, ...restProps }: ComponentProp) {
+  return (
+    <Link to={to} {...restProps}>
+      {children}
+    </Link>
+  );
+};
+
+Form.Error = function FormError({ children, ...restProps }: ComponentProp) {
+  return <Error {...restProps}>{children}</Error>;
+};
+
+Form.Spinner = function FormSpinner({ ...restProps }: ComponentProp) {
+  return <Spinner {...restProps} />;
+};
 export default Form;
